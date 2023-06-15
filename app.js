d0f8ed23f38bdd5a41a8609b391a1202311bce86
Daksh Sangal <134773611+Qu-Ack@users.noreply.github.com>
2023-06-15 11:42:14 -0400

    calculator is fully functional now except the all clear button

--- app.js
+++ app.js
@@ -1,9 +1,89 @@
-let numOne = 32;
-let numTwo = 45;
-let operator = prompt("Choose your operator !!")
+let numOne = '';
+let numTwo = '';
+let operator = '';
+let isOperatorClicked = false;
+let result = 0;
+// const one = document.querySelector(".one");
+// const two = document.querySelector(".two");
+// const three = document.querySelector(".three");
+// const four = document.querySelector(".four");
+// const five = document.querySelector(".five");
+// const six = document.querySelector(".six");
+// const seven = document.querySelector(".seven");
+// const eight = document.querySelector(".eight");
+// const nine = document.querySelector(".nine");
+// const zero = document.querySelector(".zero");
+const screen = document.querySelector(".screen");
+const numbers = document.querySelectorAll("#numbers");
+const operators = document.querySelectorAll("#operator")
+userInput(numOne)
+operatorInput()
 
 
-operate(numOne, numTwo , operator);
+
+
+
+function operatorInput() {
+    operators.forEach(op => {
+        op.addEventListener("click" , function() {
+            operate(numOne , numTwo , operator);
+            operator = op.textContent;
+            
+            if(isOperatorClicked) {
+                // numTwo = parseInt(numTwo);
+                
+                screen.textContent = result;
+                numOne = result;
+                console.log(numOne)
+                console.log(result)
+                numTwo = '';
+            } else {
+            //    numOne = parseInt(numOne);
+               isOperatorClicked = true;
+               console.log(numOne);
+            }
+            if (operator === "=") {
+                numOne = '';
+                numTwo = '';
+                isOperatorClicked = false;
+          
+            }
+            
+        })
+    })
+}
+
+
+
+function userInput(a) {
+    numbers.forEach(element => {
+        element.addEventListener('click' , function() {
+            if (isOperatorClicked) {
+                numTwo += element.textContent;
+                screen.textContent = numTwo;
+                numTwo = parseInt(numTwo)
+            } else {
+                numOne += element.textContent;
+                screen.textContent = numOne;
+                numOne = parseInt(numOne)
+
+            }
+            
+            
+        })
+       
+    })
+}
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -23,17 +103,17 @@ function operate(a , b , operator ) {
 }
 
 function add(a , b ) {
-    console.log(a +b) 
+    result = (a +b) 
 }
 
 function subtract(a, b) {
-    console.log(a-b) ;
+    result = (a-b) ;
 }
 
 function multiply(a,b) {
-    console.log(a*b) ;
+    result = (a*b) ;
 }
 
 function divide(a,b) {
-    console.log(a/b) ;
+    result = (a/b) ;
 }
